d699ea6ffa800df29f9a6ee1a5669c79ae462e41
= <callixte.girard@isep.fr>
2020-04-27 15:36:00 +0200

    okhttp3.HttpUrl integrated for building urls cleanly

--- src/main/java/callixtegirard/scrape/ScrapeDealabs.java
+++ src/main/java/callixtegirard/scrape/ScrapeDealabs.java
@@ -2,11 +2,13 @@ package callixtegirard.scrape;
 
 import static callixtegirard.util.Debug.*;
 
+import okhttp3.HttpUrl;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
+import java.net.URL;
 
 
 public class ScrapeDealabs
@@ -21,6 +23,16 @@ public class ScrapeDealabs
 
             for (int i=0 ; i<urlsSections.length ; i++)
             {
+//                URL url = HttpUrl.parse("http://example.com:4567/foldername/1234?abc=xyz").url(); // to parse
+                HttpUrl url = new HttpUrl.Builder()
+                        .scheme("https")
+                        .host("dealabs.com")
+                        .addPathSegment("hot")
+                        .addQueryParameter("page", "1")
+                        .build();
+                d(url);
+
+
                 String urlSection = urlRoot + urlsSections[i];
                 Document doc = Jsoup.connect(urlSection).get();
                 d(doc);
